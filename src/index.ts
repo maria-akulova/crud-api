82111b1413cb1dad736e7f7d2d1268dd5206cce5
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-21 23:35:58 +0200

    refactor: rename, small changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as process from 'node:process';
 import { config } from 'dotenv';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { handleUserRoutes } from './routes/userRoute';
+import { handleUserRoutes } from './routes/usersRoute';
 import {
   notFoundHandler,
   serverErrorHandler,
@@ -15,18 +15,31 @@ const requestHandler = async (
   res: ServerResponse,
 ): Promise<void> => {
   try {
+    // Here I set CORS-Headers only as a bonus in case this API might be used in browser applications
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
     const protocol: string | string[] =
       req.headers['x-forwarded-proto'] || 'http';
-    const url = new URL(req.url ?? '', `${protocol}://${req.headers.host}`);
+    const url = new URL(req.url || '', `${protocol}://${req.headers.host}`);
     const pathname: string = url.pathname.replace(/\/$/, '');
 
     const isHandled: boolean = await handleUserRoutes(req, res, pathname);
-    if (!isHandled) notFoundHandler(res);
+    if (!isHandled) {
+      notFoundHandler(res);
+      return;
+    }
   } catch {
     serverErrorHandler(res);
   }
 };
 
-const server = createServer(requestHandler);
+export const server = createServer(requestHandler);
 
 server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
