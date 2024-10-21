b59a53ff9f0c76efcadfa1ff590a777bd894b347
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-21 23:50:28 +0200

    feat: cluster

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,11 @@
-import * as process from 'node:process';
 import { config } from 'dotenv';
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import { handleUserRoutes } from './routes/usersRoute';
-import {
-  notFoundHandler,
-  serverErrorHandler,
-} from './middleware/routeHandlers';
+import * as process from 'node:process';
+import { createServer } from 'node:http';
+import { requestHandler } from './middleware/requestHandler';
 
 config();
-const PORT: string | 5001 = process.env.PORT || 5001;
 
-const requestHandler = async (
-  req: IncomingMessage,
-  res: ServerResponse,
-): Promise<void> => {
-  try {
-    // Here I set CORS-Headers only as a bonus in case this API might be used in browser applications
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-      res.writeHead(204);
-      res.end();
-      return;
-    }
-    const protocol: string | string[] =
-      req.headers['x-forwarded-proto'] || 'http';
-    const url = new URL(req.url || '', `${protocol}://${req.headers.host}`);
-    const pathname: string = url.pathname.replace(/\/$/, '');
-
-    const isHandled: boolean = await handleUserRoutes(req, res, pathname);
-    if (!isHandled) {
-      notFoundHandler(res);
-      return;
-    }
-  } catch {
-    serverErrorHandler(res);
-  }
-};
+const PORT: number = Number(process.env.PORT) || 4000;
 
 export const server = createServer(requestHandler);
 
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,21 @@ export enum HttpMethod {
   PUT = 'PUT',
   DELETE = 'DELETE',
 }
+
+export interface RequestMessage {
+  method: string | undefined;
+  url: string | null;
+  headers: Record<string, string>;
+}
+
+export interface ResponseMessage {
+  statusCode: number;
+  headers: Record<string, string>;
+  body: string;
+}
+
+export interface Message<T> {
+  action: string;
+  key: string;
+  value?: T;
+}
