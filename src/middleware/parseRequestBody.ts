01a8e6892dd2a87115a4f66433c6dd1db69ce242
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-22 02:15:22 +0200

    feat: add validation to user

--- src/middleware/parseRequestBody.ts
+++ src/middleware/parseRequestBody.ts
@@ -8,9 +8,21 @@ export const parseRequestBody = (req: IncomingMessage): Promise<IRequestBody> =>
         req.on('data', (chunk) => (body += chunk));
         req.on('end', (): void => {
             try {
-                resolve(JSON.parse(body) as IRequestBody);
+              const user = JSON.parse(body) as IRequestBody;
+
+              if (typeof user.username === 'string' ||
+                typeof user.age === 'number' ||
+                Array.isArray(user.hobbies) ||
+                (user.hobbies as string[]).every((item:string) => typeof item === 'string')){
+                  user.age = Number(user.age);
+                  user.username = user.username.trim();
+                  resolve(user);
+                } else {
+                throw Error();
+              }
+                
             } catch {
-                reject(new Error('Invalid JSON'));
+                reject(new Error('Invalid JSON. Check all fields and their types.'));
             }
         });
         req.on('error', reject);
